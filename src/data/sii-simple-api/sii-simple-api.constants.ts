1dab7447648a55723a7472398b4a8dbac3c3db44
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-05-07 14:19:26 -0400

    fix: Fixed url for simple api service.

--- src/data/sii-simple-api/sii-simple-api.constants.ts
+++ src/data/sii-simple-api/sii-simple-api.constants.ts
@@ -1,3 +1,3 @@
-export const SII_SIMPLE_API_BASE_URL = 'http://localhost:4000/proxy/sii-simple-api'; // Base URL for the API
+export const SII_SIMPLE_API_BASE_URL = 'https://dev.api.sii-apis-playground.lhpaul.cl/proxy/sii-simple-api'; // Base URL for the API
 
 export const ENVIRONMENT_VARIABLE_NOT_DEFINED_ERROR = 'SII_SIMPLE_API_KEY is not defined in the environment variables';
